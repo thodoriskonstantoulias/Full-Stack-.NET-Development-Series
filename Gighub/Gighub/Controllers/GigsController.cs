11ac1007d2acbbec0203e82cd7279cec03191c19
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-16 01:03:49 +0200

    Prevent CSRF attacks

--- Gighub/Gighub/Controllers/GigsController.cs
+++ Gighub/Gighub/Controllers/GigsController.cs
@@ -28,6 +28,7 @@ namespace Gighub.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             //var artistId = User.Identity.GetUserId();
