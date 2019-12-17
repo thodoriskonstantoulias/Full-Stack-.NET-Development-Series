2411dd25ff35373c00506c41e9ea6bce72f9bfc9
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-17 18:09:49 +0200

    Attendance table creation

--- Gighub/Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Gighub/Models/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Gighub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -19,6 +20,12 @@ namespace Gighub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>().HasRequired(a => a.Gig).WithMany().WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
